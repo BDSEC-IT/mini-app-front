713d1e23225a7d2f05e0d8113540c6a54843244e
Mongoljan Sabyrjan <mongoljansabyrjan@Mongoljans-MacBook-Pro.local>
2025-07-09 13:35:01 +0800

    changes

--- components/providers/TokenProvider.tsx
+++ components/providers/TokenProvider.tsx
@@ -4,15 +4,5 @@ import { useEffect } from 'react'
 import Cookies from 'js-cookie'
 
 export default function TokenProvider({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    // Set up demo token for testing only if no other token exists
-    const existingToken = Cookies.get('token');
-    if (!existingToken) {
-      const demoToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTYsInJvbGUiOiJVU0VSIiwidXNlcm5hbWUiOiJkaWdpcGF5IiwiaWF0IjoxNzUxOTM1Nzg5fQ.dWyKtgkHQC6pqeRaH2c7B7OY6GjhH6b29LpZ4C4oN-Y"
-      Cookies.set('token', demoToken, { expires: 7 });
-      console.log('Demo JWT token set for testing');
-    }
-  }, [])
-
   return <>{children}</>
 } 
